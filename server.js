45d8753021c5e5f79efeba279a52ecc652155dac
madhuseereddi <seereddym@gmail.com>
2025-01-18 07:43:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ function sendEmail() {
 
 // Schedule email using node-cron
 cron.schedule(
-  '25 07 * * *', // Adjust this to the desired time
+  '45 07 * * *', // Adjust this to the desired time
   () => {
     console.log('Cron job triggered at:', new Date());
     sendEmail();
